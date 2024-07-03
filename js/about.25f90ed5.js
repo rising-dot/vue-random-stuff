db6f0560be44c292ccce417ff94ee8ec2e21025a
Sornsama <sornsama@outlook.com>
2024-07-03 22:24:30 +0200

    push random stuff

---
+++ js/about.25f90ed5.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=n("7a23"),o={class:"about"},u=Object(a["f"])("h1",null," this is about page ??!!!1",-1),c=[u];function s(t,e,n,u,s,b){return Object(a["r"])(),Object(a["e"])("div",o,c)}var b={name:"About",components:{}};b.render=s;e["default"]=b}}]);
+//# sourceMappingURL=about.25f90ed5.js.map
